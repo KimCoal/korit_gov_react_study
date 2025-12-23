c30d7cf806b3a64dbfbf49fedc55ce0a7cb88a68
KimCoal <121536823+KimCoal@users.noreply.github.com>
2025-12-23 16:04:17 +0900

    Feat : React

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Emotion1 from "./Emotion/Emotion1";
 import Hook01 from "./ReactStudy/Hook01";
@@ -18,38 +19,50 @@ import React02 from "./ReactStudy/React02";
 import React03 from "./ReactStudy/React03";
 import React04 from "./ReactStudy/React04";
 import IndexPage from "./TodoList/pages/IndexPage";
+import Router01 from "./RouterStudy/Router01/Router01";
+import Router02 from "./RouterStudy/Router02/Router02";
+import Router03 from "./RouterStudy/Router03/Router03";
+import Router04 from "./RouterStudy/Router04/Router04";
 
 function App() {
     const name = "coal";
     const age = 25;
     return (
         <>
-            {/* <React01 /> */}
-            {/* <React02 /> */}
-            {/* <React03
+            <BrowserRouter>
+                {/* <React01 /> */}
+                {/* <React02 /> */}
+                {/* <React03
                 name={name}
                 age={age}
                 address={"부산시"}
                 tel={"010-1234-1234"}
             /> */}
-            {/* <React04>
+                {/* <React04>
             <h1>자식요소</h1>
             </React04> */}
-            {/* <Hook01 /> */}
-            {/* <CountState /> */}
-            {/* <Hook03 /> */}
-            {/* <Hook04 /> */}
-            {/* <Hook05 /> */}
-            {/* <Hook06 /> */}
-            {/* <Hook07 /> */}
-            {/* <Render /> */}
-            {/* <LifeCycleApp /> */}
-            {/* <FruitsFilter /> */}
-            {/* <SignupForm /> */}
-            {/* <Hook08 /> */}
-            {/* <Emotion1 /> */}
-            {/* <Hook09 /> */}
-            <IndexPage />
+                {/* <Hook01 /> */}
+                {/* <CountState /> */}
+                {/* <Hook03 /> */}
+                {/* <Hook04 /> */}
+                {/* <Hook05 /> */}
+                {/* <Hook06 /> */}
+                {/* <Hook07 /> */}
+                {/* <Render /> */}
+                {/* <LifeCycleApp /> */}
+                {/* <FruitsFilter /> */}
+                {/* <SignupForm /> */}
+                {/* <Hook08 /> */}
+                {/* <Emotion1 /> */}
+                {/* <Hook09 /> */}
+                {/* <IndexPage /> */}
+                {/* <Router01 /> */}
+                {/* <Router02 /> */}
+                {/* <Router03 /> */}
+                <Router04 />
+            </BrowserRouter>
+            {/* BrowserRouter로 감싸진 내부에 있는 컴포넌트들만 Routes, Route, Link, useNavigation같은 
+            라우터 기능을 쓸 수 있다. */}
         </>
     );
 }
---
+++ src/RouterStudy/Router01/styles.js
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const header = css`
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-around;
+    background-color: cadetblue;
+
+`;
+
+export const main = (color) => css`
+    width: 100%;
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    background-color: ${color};
+`;
+
+export const countBox = css`
+    display: flex;
+`;
